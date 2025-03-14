2b3809c0819d6de31ca0f59d36a0c5a351c6a1d4
Mateus <mateusbertuzzi@hotmail.com>
2025-03-13 23:01:19 -0300

    a

--- Main.py
+++ Main.py
@@ -704,6 +704,7 @@ def page2():
 
                 fig1, ax1 = plt.subplots()
                 for i in range(len(pressures)):
+                    st.write(len(comp),len(pressures[i][0]))
                     ax1.plot(comp,pressures[i][0],label=f'Pressure with {options[i]} model.',c=colors[i] ,zorder=3)
                 ax1.plot(comp, bubble_points, lw =2 , ls='--',c='#F08080' ,label='Bubble pressure',zorder=2)
                 ax1.set_xlabel(f'Lenght pipe [$m$]')
